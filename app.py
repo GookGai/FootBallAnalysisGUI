9e892f549a2c0a98e805ffb8e97fe27b487c2ed5
GookGai <kaiyang18110@gmail.com>
2022-06-20 16:15:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -117,32 +117,60 @@ def shotmap(data, index, mode):
     return fig
 
 
-def shotmappercent(data):
+def shotmappercent(data,k):
     cleanData(data)
-    shX = dataShot['X']
-    shY = dataShot['Y']
-    shName = dataShot['Player']
-    shsty = dataShot['Event']
-
     sout = dataShot[dataShot['Event'] == 'ShotOffTarget']
     sot = dataShot[dataShot['Event'] == 'ShotOnTarget']
     sb = dataShot[dataShot['Event'] == 'ShotBlock']
     sgg = dataShot[dataShot['Event'] == 'ShotGetGoal']
+    sout = sout.reset_index()
+    sot = sot.reset_index()
+    sb = sb.reset_index()
+    sgg = sgg.reset_index()
 
-    pitch = VerticalPitch(pitch_length=100, pitch_width=100,
-                          axis=True, pitch_color='#538053')
+    pitch = VerticalPitch(pitch_length=100, pitch_width=100,axis=True, pitch_color='#538053')
     fig, ax = pitch.draw(nrows=1,  ncols=2, figsize=(20, 10))
     bins = [(6, 5), (12, 5)]
-    for i, bin in enumerate(bins):
-        binstatistic = pitch.bin_statistic(
-            dataShot['X'], dataShot['Y'], statistic='count', bins=bin)
-        pitch.heatmap(
-            binstatistic, ax=ax[i], cmap="Oranges", edgecolors="#22312b", alpha=0.4)
-        pitch.scatter(dataShot['X'], dataShot['Y'], c='gray', ax=ax[i], s=25)
-        binstatistic['statistic'] = (pd.DataFrame((binstatistic['statistic']/binstatistic['statistic'].sum()))
-                                         .applymap(lambda x: '{:.0%}'.format(x)).values)
-        pitch.label_heatmap(binstatistic, color='black',
-                            fontsize=20, ax=ax[i], ha='center', va='bottom')
+    if k == 0:
+        for i, bin in enumerate(bins):
+            binstatistic = pitch.bin_statistic(dataShot['X'], dataShot['Y'], statistic='count', bins=bin)
+            pitch.heatmap(binstatistic, ax=ax[i], cmap="Oranges", edgecolors="#22312b", alpha=0.4)
+            pitch.scatter(dataShot['X'], dataShot['Y'],c='gray', ax=ax[i], s=25)
+            binstatistic['statistic'] = (pd.DataFrame((binstatistic['statistic']/binstatistic['statistic'].sum())).applymap(lambda x: '{:.0%}'.format(x)).values)
+            pitch.label_heatmap(binstatistic, color='black',
+                                fontsize=20, ax=ax[i], ha='center', va='bottom')
+
+    if k == 1:
+        for i, bin in enumerate(bins):
+            binstatistic = pitch.bin_statistic(sout['X'], sout['Y'], statistic='count', bins=bin)
+            pitch.heatmap(binstatistic, ax=ax[i], cmap="Oranges", edgecolors="#22312b", alpha=0.4)
+            pitch.scatter(sout['X'], sout['Y'], c='gray', ax=ax[i], s=25)
+            binstatistic['statistic'] = (pd.DataFrame((binstatistic['statistic']/binstatistic['statistic'].sum())).applymap(lambda x: '{:.0%}'.format(x)).values)
+            pitch.label_heatmap(binstatistic, color='black',fontsize=20, ax=ax[i], ha='center', va='bottom')
+
+    if k == 2:
+        for i, bin in enumerate(bins):
+            binstatistic = pitch.bin_statistic(sot['X'], sot['Y'], statistic='count', bins=bin)
+            pitch.heatmap(binstatistic, ax=ax[i], cmap="Oranges", edgecolors="#22312b", alpha=0.4)
+            pitch.scatter(sot['X'], sot['Y'], c='gray', ax=ax[i], s=25)
+            binstatistic['statistic'] = (pd.DataFrame((binstatistic['statistic']/binstatistic['statistic'].sum())).applymap(lambda x: '{:.0%}'.format(x)).values)
+            pitch.label_heatmap(binstatistic, color='black',fontsize=20, ax=ax[i], ha='center', va='bottom')
+
+    if k == 3:
+        for i, bin in enumerate(bins):
+            binstatistic = pitch.bin_statistic(sb['X'], sb['Y'], statistic='count', bins=bin)
+            pitch.heatmap(binstatistic, ax=ax[i], cmap="Oranges", edgecolors="#22312b", alpha=0.4)
+            pitch.scatter(sb['X'], sb['Y'], c='gray', ax=ax[i], s=25)
+            binstatistic['statistic'] = (pd.DataFrame((binstatistic['statistic']/binstatistic['statistic'].sum())).applymap(lambda x: '{:.0%}'.format(x)).values)
+            pitch.label_heatmap(binstatistic, color='black',fontsize=20, ax=ax[i], ha='center', va='bottom')
+    if k == 4:
+        for i, bin in enumerate(bins):
+            binstatistic = pitch.bin_statistic(sgg['X'], sgg['Y'], statistic='count', bins=bin)
+            pitch.heatmap(binstatistic, ax=ax[i], cmap="Oranges", edgecolors="#22312b", alpha=0.4)
+            pitch.scatter(sgg['X'], sgg['Y'], c='gray', ax=ax[i], s=25)
+            binstatistic['statistic'] = (pd.DataFrame((binstatistic['statistic']/binstatistic['statistic'].sum())).applymap(lambda x: '{:.0%}'.format(x)).values)
+            pitch.label_heatmap(binstatistic, color='black',fontsize=20, ax=ax[i], ha='center', va='bottom')
+
     title = fig.suptitle('Attacking Area', fontsize=50)
     fileName = 'AttackingArea'
     plt.savefig(fileName + '.png')
@@ -169,8 +197,8 @@ def genGraph01(file_index, t, v):
     window["-IMAGE-"].update(data=bio.getvalue())
 
 
-def genGraph02(file_index):
-    p = shotmappercent(pd.read_csv(file_index))
+def genGraph02(file_index,k):
+    p = shotmappercent(pd.read_csv(file_index),k)
     filenames = os.path.join("AttackingArea.png")
     # image = Image.open(filename)
     # image.thumbnail((400,400))
@@ -183,37 +211,70 @@ def genGraph02(file_index):
 
 def GraphPosition():
     if values["FILE_LIST"][0] == "ภาพรวม":
-        try:
-            genGraph01(file_index, 1, 0)
-        except:
-            sg.popup("please check your data Input")
+        if values["MODE_LIST"][0] == "เปอร์เซ็นต์":
+            try:
+                genGraph02(file_index,0)
+            except:
+                sg.popup("please check your data Input2")
+        else:
+            try:
+                genGraph01(file_index, 1, 0)
+            except:
+                sg.popup("please check your data Input")
 
     if values["FILE_LIST"][0] == "ได้ประตู":
-        try:
-            genGraph01(file_index, 1, 4)
-        except:
-            sg.popup("please check your data Input")
+        if values["MODE_LIST"][0] == "เปอร์เซ็นต์":
+            try:
+                genGraph02(file_index,4)
+            except:
+                sg.popup("please check your data Input2")
+        else:
+            try:
+                genGraph01(file_index, 1, 4)
+            except:
+                sg.popup("please check your data Input")
+
     if values["FILE_LIST"][0] == "ยิงเข้ากรอบ":
-        try:
-            genGraph01(file_index, 1, 2)
-        except:
-            sg.popup("please check your data Input")
+        if values["MODE_LIST"][0] == "เปอร์เซ็นต์":
+            try:
+                genGraph02(file_index,2)
+            except:
+                sg.popup("please check your data Input2")
+        else:
+            try:
+                genGraph01(file_index, 1, 2)
+            except:
+                sg.popup("please check your data Input")
+
     if values["FILE_LIST"][0] == "ยิงออก":
-        try:
-            genGraph01(file_index, 1, 1)
-        except:
-            sg.popup("please check your data Input")
+        if values["MODE_LIST"][0] == "เปอร์เซ็นต์":
+            try:
+                genGraph02(file_index,1)
+            except:
+                sg.popup("please check your data Input2")
+        else:
+            try:
+                genGraph01(file_index, 1, 1)
+            except:
+                sg.popup("please check your data Input")
+
     if values["FILE_LIST"][0] == "ยิงติดบล็อก":
-        try:
-            genGraph01(file_index, 1, 3)
-        except:
-            sg.popup("please check your data Input")
+        if values["MODE_LIST"][0] == "เปอร์เซ็นต์":
+            try:
+                genGraph02(file_index,3)
+            except:
+                sg.popup("please check your data Input2")
+        else:
+            try:
+                genGraph01(file_index, 1, 3)
+            except:
+                sg.popup("please check your data Input")
 
 
 ####################################################################################################################################
 ####################################################################################################################################
 ####################################################################################################################################
-sg.theme('Black')
+sg.theme('Kayak')
 file_layout = [
     [
         sg.Text("Choose file"),
@@ -222,7 +283,7 @@ file_layout = [
     ],
     [
         sg.Listbox(
-            values=["ภาพรวม", "ได้ประตู", "ยิงเข้ากรอบ", "ยิงออก", "ยิงติดบล็อก"], enable_events=True, size=(30, 10), key="FILE_LIST"
+            values=["ภาพรวม", "ได้ประตู", "ยิงเข้ากรอบ", "ยิงออก", "ยิงติดบล็อก"], enable_events=True, size=(30, 12), key="FILE_LIST"
         ),
 
     ],
@@ -232,7 +293,7 @@ file_layout = [
     [
 
         sg.Listbox(
-            values=["ตำแหน่ง","เปอร์เซ็นต์"], enable_events=True, size=(30, 2), key="MODE_LIST"
+            values=["ตำแหน่ง", "เปอร์เซ็นต์"], enable_events=True, size=(30, 2), key="MODE_LIST"
         ),
 
     ],
@@ -242,10 +303,11 @@ file_layout = [
     [
 
         sg.Listbox(
-            values=[], enable_events=True, size=(30, 100), key="NAME_LIST"
+            values=[], enable_events=True, size=(30, 15), key="NAME_LIST"
         ),
 
-    ]
+    ],
+    [sg.Button("ทั้งหมด", size=(25, 1))],
 ]
 
 # mode_sel =[
@@ -271,7 +333,7 @@ layout = [
     ]
 ]
 
-window = sg.Window("Gaiyang", layout, size=(1300, 700),resizable=True)
+window = sg.Window("Gaiyang", layout, size=(1300, 700), resizable=True)
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
@@ -290,16 +352,16 @@ while True:
         window["NAME_LIST"].update(playerList)
 
     if event == "FILE_LIST":
-        GraphPosition()
+        try:
+            GraphPosition()
+        except:
+            sg.popup("please check your data Input")
             # window["-IMAGE-"].update(filename = filenames )
     # if event == "FILE_LIST":
     #     sg.popup('You entered')
-    if event  == "MODE_LIST":
-        if values["MODE_LIST"][0] == "ตำแหน่ง":
+    if event == "MODE_LIST":
+        try:
             GraphPosition()
-        if values["MODE_LIST"][0] == "เปอร์เซ็นต์":
-            try:
-                genGraph02(file_index )
-            except:
-                sg.popup("please check your data Input")
+        except:
+            sg.popup("please check your data Input")
 window.close()
