2bd10426fee030fcd618334bfcdb2ef43a85597b
GookGai <kaiyang18110@gmail.com>
2022-06-24 23:59:09 +0700

    app

--- vle.py
+++ vle.py
@@ -67,7 +67,7 @@ def cleanData(rawdata):
     return(data, data15, data30, data45, dataShot, dataPass, dataPass15, dataPass30, dataPass45,cl,cr)
 
 
-def shotmap(data1, index, mode):
+def shotmap(data1, mode):
     cleanData(data1)
     shX = dataShot['X']
     shY = dataShot['Y']
@@ -78,58 +78,15 @@ def shotmap(data1, index, mode):
     sot = dataShot[dataShot['Event'] == 'ShotOnTarget']
     sb = dataShot[dataShot['Event'] == 'ShotBlock']
     sgg = dataShot[dataShot['Event'] == 'ShotGetGoal']
-    # cl = data[data['Event'] == 'CornerLeft']
-    # cr = data[data['Event'] == 'CornerRight']
 
-    pitch = VerticalPitch(pitch_length=100, pitch_width=100,
-                          axis=True, pitch_color='#538053', half=True)
-    fig, ax = pitch.draw(nrows=1,  ncols=1, figsize=(25,12))
-
-    # plt.scatter(dataShot['Y'],dataShot['X'],c='#BE2653',s = 700,marker = "*")
-    if mode == 0:
-        plt.scatter(sout['Y'], sout['X'], c='#fcf5e1', s=800, marker="o")
-        plt.scatter(sot['Y'], sot['X'], c='#1E2453', s=800, marker="o")
-        plt.scatter(sb['Y'], sb['X'], c='#1DD473', s=800, marker="P")
-        plt.scatter(sgg['Y'], sgg['X'], c='#BF211E', s=800, marker="*")
-        title = fig.suptitle('Total Shots', fontsize=40)
-        for i in range(len(dataShot['X'])):
-            plt.text(shY[i]-1, shX[i]-2.5, shName[i], fontsize=15)
-    if mode == 1:
-        plt.scatter(sout['Y'], sout['X'], c='#fcf5e1', s=800, marker="o")
-        title = fig.suptitle('Wide', fontsize=40)
-        for i in range(len(dataShot['X'])):
-            if shsty[i] == 'ShotOffTarget':
-                plt.text(shY[i]-1, shX[i]-2.5, shName[i], fontsize=15)
-    if mode == 2:
-        plt.scatter(sot['Y'], sot['X'], c='#1E2453', s=800, marker="o")
-        title = fig.suptitle('On goal', fontsize=40)
-        for i in range(len(dataShot['X'])):
-            if shsty[i] == 'ShotOnTarget':
-                plt.text(shY[i]-1, shX[i]-2.5, shName[i], fontsize=15)
-    if mode == 3:
-        plt.scatter(sb['Y'], sb['X'], c='#1E2453', s=800, marker="o")
-        title = fig.suptitle('Blocked', fontsize=40)
-        for i in range(len(dataShot['X'])):
-            if shsty[i] == 'ShotBlock':
-                plt.text(shY[i]-1, shX[i]-2.5, shName[i], fontsize=15)
-    if mode == 4:
-        plt.scatter(sgg['Y'], sgg['X'], c='#1E2453', s=800, marker="*")
-        title = fig.suptitle('Goal', fontsize=40)
-        for i in range(len(dataShot['X'])):
-            if shsty[i] == 'ShotGetGoal':
-                plt.text(shY[i]-1, shX[i]-2.5, shName[i], fontsize=15)
-    if mode == 5:
-        plt.scatter(cl['Y'], cl['X'], c='#1E2453', s=800, marker="o")
-        title = fig.suptitle('CornerLeft', fontsize=40)
-    if mode == 6:
-        plt.scatter(cr['Y'], cr['X'], c='#1E2453', s=800, marker="o")
-        title = fig.suptitle('CornerRight', fontsize=40)
-#     ax[1].plt.scatter(sout['Y'],sout['X'],c='#fcf5e1',s = 400,marker = "o")
-
-    # title = fig.suptitle('Attacking' + str(index), fontsize=50)
-    fileName = 'Attacking' + str(index)
-    plt.savefig(fileName + '.png')
-    return fig
+    img = plt.imread("vll.jpg")
+    ext = [0.0, 120.0, 0.00, 80.0]
+    plt.scatter(dataShot['X'],dataShot['Y'],s = 150, alpha=0.8,cmap='viridis')
+    plt.scatter(dataShot['X'],dataShot['Y'],s = 150, alpha=0.8,cmap='viridis')
+    plt.imshow(img, extent=ext ,aspect='auto')
+    fileName = 'Vle Attacking'
+    c = plt.savefig(fileName + '.png')
+    return c
 
 
 def shotmappercent(data,k):
@@ -154,53 +111,6 @@ def shotmappercent(data,k):
             binstatistic['statistic'] = (pd.DataFrame((binstatistic['statistic']/binstatistic['statistic'].sum())).applymap(lambda x: '{:.0%}'.format(x)).values)
             pitch.label_heatmap(binstatistic, color='black',
                                 fontsize=20, ax=ax[i], ha='center', va='bottom')
-
-    if k == 1:
-        for i, bin in enumerate(bins):
-            binstatistic = pitch.bin_statistic(sout['X'], sout['Y'], statistic='count', bins=bin)
-            pitch.heatmap(binstatistic, ax=ax[i], cmap="Oranges", edgecolors="#22312b", alpha=0.4)
-            pitch.scatter(sout['X'], sout['Y'], c='gray', ax=ax[i], s=25)
-            binstatistic['statistic'] = (pd.DataFrame((binstatistic['statistic']/binstatistic['statistic'].sum())).applymap(lambda x: '{:.0%}'.format(x)).values)
-            pitch.label_heatmap(binstatistic, color='black',fontsize=20, ax=ax[i], ha='center', va='bottom')
-
-    if k == 2:
-        for i, bin in enumerate(bins):
-            binstatistic = pitch.bin_statistic(sot['X'], sot['Y'], statistic='count', bins=bin)
-            pitch.heatmap(binstatistic, ax=ax[i], cmap="Oranges", edgecolors="#22312b", alpha=0.4)
-            pitch.scatter(sot['X'], sot['Y'], c='gray', ax=ax[i], s=25)
-            binstatistic['statistic'] = (pd.DataFrame((binstatistic['statistic']/binstatistic['statistic'].sum())).applymap(lambda x: '{:.0%}'.format(x)).values)
-            pitch.label_heatmap(binstatistic, color='black',fontsize=20, ax=ax[i], ha='center', va='bottom')
-
-    if k == 3:
-        for i, bin in enumerate(bins):
-            binstatistic = pitch.bin_statistic(sb['X'], sb['Y'], statistic='count', bins=bin)
-            pitch.heatmap(binstatistic, ax=ax[i], cmap="Oranges", edgecolors="#22312b", alpha=0.4)
-            pitch.scatter(sb['X'], sb['Y'], c='gray', ax=ax[i], s=25)
-            binstatistic['statistic'] = (pd.DataFrame((binstatistic['statistic']/binstatistic['statistic'].sum())).applymap(lambda x: '{:.0%}'.format(x)).values)
-            pitch.label_heatmap(binstatistic, color='black',fontsize=20, ax=ax[i], ha='center', va='bottom')
-    if k == 4:
-        for i, bin in enumerate(bins):
-            binstatistic = pitch.bin_statistic(sgg['X'], sgg['Y'], statistic='count', bins=bin)
-            pitch.heatmap(binstatistic, ax=ax[i], cmap="Oranges", edgecolors="#22312b", alpha=0.4)
-            pitch.scatter(sgg['X'], sgg['Y'], c='gray', ax=ax[i], s=25)
-            binstatistic['statistic'] = (pd.DataFrame((binstatistic['statistic']/binstatistic['statistic'].sum())).applymap(lambda x: '{:.0%}'.format(x)).values)
-            pitch.label_heatmap(binstatistic, color='black',fontsize=20, ax=ax[i], ha='center', va='bottom')
-    if k == 5:
-        for i, bin in enumerate(bins):
-            binstatistic = pitch.bin_statistic(cl['X'], cl['Y'], statistic='count', bins=bin)
-            pitch.heatmap(binstatistic, ax=ax[i], cmap="Oranges", edgecolors="#22312b", alpha=0.4)
-            pitch.scatter(cl['X'], cl['Y'], c='gray', ax=ax[i], s=25)
-            binstatistic['statistic'] = (pd.DataFrame((binstatistic['statistic']/binstatistic['statistic'].sum())).applymap(lambda x: '{:.0%}'.format(x)).values)
-            pitch.label_heatmap(binstatistic, color='black',fontsize=20, ax=ax[i], ha='center', va='bottom')
-    if k == 6:
-        for i, bin in enumerate(bins):
-            binstatistic = pitch.bin_statistic(cr['X'], cr['Y'], statistic='count', bins=bin)
-            pitch.heatmap(binstatistic, ax=ax[i], cmap="Oranges", edgecolors="#22312b", alpha=0.4)
-            pitch.scatter(cr['X'], cr['Y'], c='gray', ax=ax[i], s=25)
-            binstatistic['statistic'] = (pd.DataFrame((binstatistic['statistic']/binstatistic['statistic'].sum())).applymap(lambda x: '{:.0%}'.format(x)).values)
-            pitch.label_heatmap(binstatistic, color='black',fontsize=20, ax=ax[i], ha='center', va='bottom')
-
-
     title = fig.suptitle('Attacking Area', fontsize=50)
     fileName = 'AttackingArea'
     plt.savefig(fileName + '.png')
@@ -215,9 +125,9 @@ def genname(data):
 ####################################################################################################################################
 
 
-def genGraph01(file_index, t, v):
-    p = shotmap(pd.read_csv(file_index), t, v)
-    filenames = os.path.join("Attacking1.png")
+def genGraph01(file_index, v):
+    p = shotmap(pd.read_csv(file_index), v)
+    filenames = os.path.join("Vle Attacking.png")
     # image = Image.open(filename)
     # image.thumbnail((400,400))
     image = Image.open(filenames)
@@ -248,84 +158,84 @@ def GraphPosition():
                 sg.popup("please check your data Input2")
         else:
             try:
-                genGraph01(file_index, 1, 0)
+                genGraph01(file_index,0)
             except:
                 sg.popup("please check your data Input")
 
-    if values["FILE_LIST"][0] == "ได้ประตู":
-        if values["MODE_LIST"][0] == "เปอร์เซ็นต์":
-            try:
-                genGraph02(file_index,4)
-            except:
-                sg.popup("please check your data Input2")
-        else:
-            try:
-                genGraph01(file_index, 1, 4)
-            except:
-                sg.popup("please check your data Input")
+    # if values["FILE_LIST"][0] == "ได้ประตู":
+    #     if values["MODE_LIST"][0] == "เปอร์เซ็นต์":
+    #         try:
+    #             genGraph02(file_index,4)
+    #         except:
+    #             sg.popup("please check your data Input2")
+    #     else:
+    #         try:
+    #             genGraph01(file_index, 1, 4)
+    #         except:
+    #             sg.popup("please check your data Input")
 
-    if values["FILE_LIST"][0] == "ยิงเข้ากรอบ":
-        if values["MODE_LIST"][0] == "เปอร์เซ็นต์":
-            try:
-                genGraph02(file_index,2)
-            except:
-                sg.popup("please check your data Input2")
-        else:
-            try:
-                genGraph01(file_index, 1, 2)
-            except:
-                sg.popup("please check your data Input")
+    # if values["FILE_LIST"][0] == "ยิงเข้ากรอบ":
+    #     if values["MODE_LIST"][0] == "เปอร์เซ็นต์":
+    #         try:
+    #             genGraph02(file_index,2)
+    #         except:
+    #             sg.popup("please check your data Input2")
+    #     else:
+    #         try:
+    #             genGraph01(file_index, 1, 2)
+    #         except:
+    #             sg.popup("please check your data Input")
 
-    if values["FILE_LIST"][0] == "ยิงออก":
-        if values["MODE_LIST"][0] == "เปอร์เซ็นต์":
-            try:
-                genGraph02(file_index,1)
-            except:
-                sg.popup("please check your data Input2")
-        else:
-            try:
-                genGraph01(file_index, 1, 1)
-            except:
-                sg.popup("please check your data Input")
+    # if values["FILE_LIST"][0] == "ยิงออก":
+    #     if values["MODE_LIST"][0] == "เปอร์เซ็นต์":
+    #         try:
+    #             genGraph02(file_index,1)
+    #         except:
+    #             sg.popup("please check your data Input2")
+    #     else:
+    #         try:
+    #             genGraph01(file_index, 1, 1)
+    #         except:
+    #             sg.popup("please check your data Input")
 
-    if values["FILE_LIST"][0] == "ยิงติดบล็อก":
-        if values["MODE_LIST"][0] == "เปอร์เซ็นต์":
-            try:
-                genGraph02(file_index,3)
-            except:
-                sg.popup("please check your data Input2")
-        else:
-            try:
-                genGraph01(file_index, 1, 3)
-            except:
-                sg.popup("please check your data Input")
+    # if values["FILE_LIST"][0] == "ยิงติดบล็อก":
+    #     if values["MODE_LIST"][0] == "เปอร์เซ็นต์":
+    #         try:
+    #             genGraph02(file_index,3)
+    #         except:
+    #             sg.popup("please check your data Input2")
+    #     else:
+    #         try:
+    #             genGraph01(file_index, 1, 3)
+    #         except:
+    #             sg.popup("please check your data Input")
 
-    if values["FILE_LIST"][0] == "เตะมุมซ้าย":
-        if values["MODE_LIST"][0] == "เปอร์เซ็นต์":
-            try:
-                genGraph02(file_index,5)
-            except:
-                sg.popup("please check your data Input2")
-        else:
-            try:
-                genGraph01(file_index, 1, 5)
-            except:
-                sg.popup("please check your data Input")
+    # if values["FILE_LIST"][0] == "เตะมุมซ้าย":
+    #     if values["MODE_LIST"][0] == "เปอร์เซ็นต์":
+    #         try:
+    #             genGraph02(file_index,5)
+    #         except:
+    #             sg.popup("please check your data Input2")
+    #     else:
+    #         try:
+    #             genGraph01(file_index, 1, 5)
+    #         except:
+    #             sg.popup("please check your data Input")
     
-    if values["FILE_LIST"][0] == "เตะมุมขวา":
-        if values["MODE_LIST"][0] == "เปอร์เซ็นต์":
-            try:
-                genGraph02(file_index,6)
-            except:
-                sg.popup("please check your data Input2")
-        else:
-            try:
-                genGraph01(file_index, 1, 6)
-            except:
-                sg.popup("please check your data Input")
+    # if values["FILE_LIST"][0] == "เตะมุมขวา":
+    #     if values["MODE_LIST"][0] == "เปอร์เซ็นต์":
+    #         try:
+    #             genGraph02(file_index,6)
+    #         except:
+    #             sg.popup("please check your data Input2")
+    #     else:
+    #         try:
+    #             genGraph01(file_index, 1, 6)
+    #         except:
+    #             sg.popup("please check your data Input")
     
-    if values["FILE_LIST"][0] == "เตะมุมขวา":
-        sg.popup("ยังไม่ได้ทำ")
+    # if values["FILE_LIST"][0] == "เตะมุมขวา":
+    #     sg.popup("ยังไม่ได้ทำ")
 
 
 
